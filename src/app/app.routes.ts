b004e8417a396321b763c95731365ae6a3c5a3c7
Adrian <adrianslt2000@gmail.com>
2024-04-25 18:23:42 +0200

    Home funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'home',
+        loadComponent: () =>
+            import('./components/home/home.component').then(m => m.HomeComponent)
+    },
+    {
         path: 'listaProductos',
         loadComponent: () => 
             import('./components/lista-productos/lista-productos.component').then(m => m.ListaProductosComponent),    
@@ -13,7 +18,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        redirectTo: 'listaProductos',
+        redirectTo: 'home',
         pathMatch: 'full'
     }
 ];
