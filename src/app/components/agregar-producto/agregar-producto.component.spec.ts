6c2f36123ec2c3ea4645788e66be98eea76ee7c7
Adrian <adrianslt2000@gmail.com>
2024-10-16 18:04:30 +0200

    Tests para alta de nuevo producto, fallan los de cantidad y precio negativo

--- src/app/components/agregar-producto/agregar-producto.component.spec.ts
+++ src/app/components/agregar-producto/agregar-producto.component.spec.ts
@@ -1,133 +1,159 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { AgregarProductoComponent } from './agregar-producto.component';
 import { ProductoService } from '../../services/producto.service';
-import { By } from '@angular/platform-browser';
+import { of, throwError } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AgregarProductoComponent', () => {
   let component: AgregarProductoComponent;
   let fixture: ComponentFixture<AgregarProductoComponent>;
-  let productoService: ProductoService;
+  let productoServiceMock: jasmine.SpyObj<ProductoService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        AgregarProductoComponent,
+  beforeEach(() => {
+    productoServiceMock = jasmine.createSpyObj('ProductoService', ['crearProducto']);
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: ProductoService, useValue: productoServiceMock }],
+      imports: [AgregarProductoComponent,
         FormsModule,
         ReactiveFormsModule,
-        HttpClientModule
-      ],
-      providers: [
-        ProductoService
-      ]
-    })
-      .compileComponents();
+        HttpClientModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AgregarProductoComponent);
     component = fixture.componentInstance;
-    productoService = TestBed.inject(ProductoService);
-    fixture.detectChanges();
   });
 
+  describe('Prueba de éxito', () => {
+    it('Debería añadir un producto con éxito', () => {
+      component.producto = {
+        nombre: 'Producto Test',
+        descripcion: 'Descripción Test',
+        precio: 100,
+        stock: 10
+      };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+      productoServiceMock.crearProducto.and.returnValue(of({ success: true }));
+
+      const form: NgForm = {
+        valid: true,
+        value: component.producto,
+        controls: {},
+        resetForm: () => { }
+      } as unknown as NgForm;
+
+      spyOn(component, 'resetForm');
+      component.agregarProducto(form);
+
+      expect(productoServiceMock.crearProducto).toHaveBeenCalledWith(component.producto);
+      expect(component.resetForm).toHaveBeenCalled();
+    });
   });
 
+  describe('Prueba de error por campo vacío', () => {
+    it('Debería resaltar los campos vacíos y mostrar mensaje de error', () => {
+      const form: NgForm = {
+        valid: false,
+        controls: {
+          nombre: jasmine.createSpyObj('Control', ['markAsTouched']),
+          precio: jasmine.createSpyObj('Control', ['markAsTouched']),
+          stock: jasmine.createSpyObj('Control', ['markAsTouched']),
+        },
+        resetForm: () => { }
+      } as unknown as NgForm;
 
-  it('Prueba de éxito', () => {
-    const compiled = fixture.nativeElement;
-    const nombreInput = compiled.querySelector('input[name="nombre"]');
-    const precioInput = compiled.querySelector('input[name="precio"]');
-    const cantidadInput = compiled.querySelector('input[name="stock"]');
+      component.agregarProducto(form);
 
-    // Simulación de la entrada para el nombre del producto
-    nombreInput.value = 'Nuevo Producto';
-    nombreInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para el precio del producto
-    precioInput.value = 50;
-    precioInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para la cantidad del producto
-    cantidadInput.value = 100;
-    cantidadInput.dispatchEvent(new Event('input'));
-
-    // Comprobación de los valores dentro del componente
-    expect(component.producto.nombre).toEqual('Nuevo Producto');
-    expect(component.producto.precio).toEqual(50);
-    expect(component.producto.stock).toEqual(100);
+      expect(form.controls['nombre'].markAsTouched).toHaveBeenCalled();
+      expect(form.controls['precio'].markAsTouched).toHaveBeenCalled();
+      expect(form.controls['stock'].markAsTouched).toHaveBeenCalled();
+    });
   });
 
+  describe('Prueba de error por duplicidad', () => {
+    it('Debería mostrar un mensaje de error por producto duplicado', () => {
+      component.producto = {
+        nombre: 'Producto Duplicado',
+        descripcion: 'Descripción Duplicado',
+        precio: 100,
+        stock: 10
+      };
 
-  it('Prueba de error por campo vacío', () => {
-    component.producto.nombre = '';
-    component.producto.precio = 50;
-    component.producto.stock = 10;
+      productoServiceMock.crearProducto.and.returnValue(throwError({ status: 409 }));
 
-    // Simula la acción de enviar el formulario
-    let form = fixture.debugElement.query(By.css('form'));
-    form.triggerEventHandler('submit', null);
+      const form: NgForm = {
+        valid: true,
+        value: component.producto,
+        controls: {},
+        resetForm: () => { }
+      } as unknown as NgForm;
 
-    fixture.detectChanges();
-
-    // Verifica si se muestra el mensaje de error
-    let errorMsg = fixture.debugElement.query(By.css('.error'));
-    expect(errorMsg).toBeTruthy();
-    expect(errorMsg.nativeElement.textContent).toContain('El nombre del producto es obligatorio.' || 'El precio es obligatorio.' || 'La cantidad es obligatoria.');
+      component.agregarProducto(form);
+      expect(component.errorMessage).toBe('Error al agregar el producto, inténtalo de nuevo.');
+    });
   });
 
+  describe('Prueba de precio negativo', () => {
+    it('Debería mostrar un mensaje de error por precio negativo', () => {
+      component.producto = {
+        nombre: 'Producto Test',
+        descripcion: 'Descripción Test',
+        precio: -50,
+        stock: 10
+      };
 
-  it('Prueba de precio negativo', () => {
-    const compiled = fixture.nativeElement;
-    const nombreInput = compiled.querySelector('input[name="nombre"]');
-    const precioInput = compiled.querySelector('input[name="precio"]');
-    const cantidadInput = compiled.querySelector('input[name="stock"]');
+      const form: NgForm = {
+        valid: false,
+        controls: {},
+        resetForm: () => { }
+      } as unknown as NgForm;
 
-    // Simulación de la entrada para el nombre del producto
-    nombreInput.value = 'Nuevo Producto';
-    nombreInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para el precio del producto
-    precioInput.value = -50;
-    precioInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para la cantidad del producto
-    cantidadInput.value = 100;
-    cantidadInput.dispatchEvent(new Event('input'));
-
-    fixture.detectChanges();
-
-    // Busca el mensaje de error
-    const error = fixture.debugElement.query(By.css('#error-precio'));
-    expect(error.nativeElement.innerText).toContain('El precio debe ser mayor que 0');
+      component.agregarProducto(form);
+      expect(component.errorMessage).toBe('El precio debe ser mayor que 0.');
+    });
   });
 
+  describe('Prueba de cantidad negativa', () => {
+    it('Debería mostrar un mensaje de error por stock no positivo', () => {
+      component.producto = {
+        nombre: 'Producto Test',
+        descripcion: 'Descripción Test',
+        precio: 100,
+        stock: 0,
+      };
 
-  it('Prueba de cantidad negativa', () => {
-    const compiled = fixture.nativeElement;
-    const nombreInput = compiled.querySelector('input[name="nombre"]');
-    const precioInput = compiled.querySelector('input[name="precio"]');
-    const cantidadInput = compiled.querySelector('input[name="stock"]');
+      const form: NgForm = {
+        valid: false,
+        controls: {},
+        resetForm: () => { }
+      } as unknown as NgForm;
 
-    // Simulación de la entrada para el nombre del producto
-    nombreInput.value = 'Nuevo Producto';
-    nombreInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para el precio del producto
-    precioInput.value = 50;
-    precioInput.dispatchEvent(new Event('input'));
-
-    // Simulación de la entrada para la cantidad del producto
-    cantidadInput.value = -10;
-    cantidadInput.dispatchEvent(new Event('input'));
-
-    fixture.detectChanges();
-
-    // Busca el mensaje de error
-    const error = fixture.debugElement.query(By.css('#error-cantidad'));
-    expect(error.nativeElement.innerText).toContain('La cantidad mínima es 1');
+      component.agregarProducto(form);
+      expect(component.errorMessage).toBe('La cantidad mínima es 1.');
+    });
   });
 
+  describe('Fallo en la base de datos', () => {
+    it('Debería mostrar un mensaje de error en caso de fallo de base de datos', () => {
+      component.producto = {
+        nombre: 'Producto Test',
+        descripcion: 'Descripción Test',
+        precio: 100,
+        stock: 10
+      };
+
+      productoServiceMock.crearProducto.and.returnValue(throwError({ status: 500 }));
+
+      const form: NgForm = {
+        valid: true,
+        value: component.producto,
+        controls: {},
+        resetForm: () => { }
+      } as unknown as NgForm;
+
+      component.agregarProducto(form);
+      expect(component.errorMessage).toBe('Error al agregar el producto, inténtalo de nuevo.');
+    });
+  });
 });
