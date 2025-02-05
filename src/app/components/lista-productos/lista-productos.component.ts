90e39c2c67aa319928eee31cc5f560fb9a9bd07c
adriansanluis2000 <adrian.san-luis@alumnos.unican.es>
2025-02-05 16:02:27 +0100

    Solucionado error en la búsqueda de productos por nombre

--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from '../../services/producto.service';
 import { NgFor, NgIf } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-productos',
@@ -18,18 +12,15 @@ import {
 })
 export class ListaProductosComponent implements OnInit {
   productos: any[] = [];
-  productosOriginales: any[] = [];
+  productosFiltrados: any[] = [];
   productoSeleccionado: any;
   productoForm: FormGroup;
   ordenAscendente: boolean = true;
-
+  
   errorMessage: string = '';
   busqueda: string = '';
 
-  constructor(
-    private readonly productoService: ProductoService,
-    private readonly fb: FormBuilder
-  ) {
+  constructor(private readonly productoService: ProductoService, private readonly fb: FormBuilder) {
     this.productoForm = this.fb.group({
       nombre: ['', Validators.required],
       descripcion: [''],
@@ -40,7 +31,6 @@ export class ListaProductosComponent implements OnInit {
 
   ngOnInit(): void {
     this.obtenerProductos();
-    this.productosOriginales = [...this.productos];
   }
 
   filtrarProductos(): void {
@@ -49,20 +39,12 @@ export class ListaProductosComponent implements OnInit {
       .map((termino) => termino.trim().toLowerCase())
       .filter((termino) => termino !== '');
 
-    // Restauramos la lista de productos a la lista original antes de filtrar
-    this.productos = [...this.productosOriginales];
-
-    // Filtramos solo si hay términos de búsqueda
-    if (terminos.length > 0) {
-      this.productos = this.productos.filter((producto) =>
-        terminos.every((termino) =>
-          producto.nombre.toLowerCase().includes(termino)
-        )
-      );
-    }
+    this.productosFiltrados = this.productos.filter((producto) =>
+      terminos.every((termino) => producto.nombre.toLowerCase().includes(termino))
+    );
 
     // Mostramos el mensaje de error si no hay productos encontrados
-    if (this.productos.length === 0) {
+    if (this.productosFiltrados.length === 0) {
       this.errorMessage = 'No se encontraron productos.';
     } else {
       this.errorMessage = '';
@@ -72,13 +54,12 @@ export class ListaProductosComponent implements OnInit {
   obtenerProductos(): void {
     this.productoService.obtenerProductos().subscribe({
       next: (data: any[]) => {
-        this.productosOriginales = data; // Guarda los productos originales
-        this.productos = this.filteredProducts; // Asigna los productos filtrados a productos
+        this.productos = data;
+        this.productosFiltrados = [...this.productos];
       },
       error: (e) => {
         if (e.status === 0) {
-          this.errorMessage =
-            'Error de conexión. Verifica tu conexión a internet y vuelve a intentarlo.';
+          this.errorMessage = 'Error de conexión. Verifica tu conexión a internet y vuelve a intentarlo.';
         } else {
           this.errorMessage = 'Error al obtener productos';
         }
@@ -87,15 +68,11 @@ export class ListaProductosComponent implements OnInit {
   }
 
   eliminarProducto(id: number): void {
-    const confirmacion = window.confirm(
-      '¿Estás seguro de que deseas eliminar este producto?'
-    );
+    const confirmacion = window.confirm('¿Estás seguro de que deseas eliminar este producto?');
     if (confirmacion) {
       this.productoService.eliminarProducto(id).subscribe({
         next: () => {
-          this.productos = this.productos.filter(
-            (producto) => producto.id !== id
-          );
+          this.productos = this.productos.filter((producto) => producto.id !== id);
         },
         error: (e) => {
           console.error('Error al eliminar producto', e);
@@ -126,31 +103,16 @@ export class ListaProductosComponent implements OnInit {
         ...this.productoForm.value,
       };
 
-      this.productoService
-        .actualizarProducto(productoActualizado.id, productoActualizado)
-        .subscribe({
-          next: () => {
-            // Actualizamos los arrays de productos
-            const indexOriginal = this.productosOriginales.findIndex(
-              (prod) => prod.id === productoActualizado.id
-            );
-            const indexFiltered = this.productos.findIndex(
-              (prod) => prod.id === productoActualizado.id
-            );
-
-            if (indexOriginal !== -1) {
-              this.productosOriginales[indexOriginal] = productoActualizado;
-            }
-            if (indexFiltered !== -1) {
-              this.productos[indexFiltered] = productoActualizado;
-            }
-
-            this.cerrarModal();
-          },
-          error: (e) => {
-            console.error('Error al actualizar producto', e);
-          },
-        });
+      this.productoService.actualizarProducto(productoActualizado.id, productoActualizado).subscribe({
+        next: () => {
+          console.log('any actualizado con éxito');
+          this.cerrarModal();
+          this.obtenerProductos();
+        },
+        error: (e) => {
+          console.error('Error al actualizar producto', e);
+        },
+      });
     } else {
       console.log('Formulario no válido o producto no seleccionado');
     }
@@ -160,9 +122,7 @@ export class ListaProductosComponent implements OnInit {
     this.productos.sort((a, b) => {
       const nombreA = a.nombre.toLowerCase();
       const nombreB = b.nombre.toLowerCase();
-      return this.ordenAscendente
-        ? nombreA.localeCompare(nombreB)
-        : nombreB.localeCompare(nombreA);
+      return this.ordenAscendente ? nombreA.localeCompare(nombreB) : nombreB.localeCompare(nombreA);
     });
     this.ordenAscendente = !this.ordenAscendente;
   }
@@ -183,24 +143,24 @@ export class ListaProductosComponent implements OnInit {
     stock: '', // Puede ser 'low', 'near-threshold' o vacío
   };
 
-  get filteredProducts() {
+  filtrarPorStock() {
     const porcentajeCercania = 20; // Porcentaje de cercanía al umbral
 
-    return this.productosOriginales.filter((producto) => {
+    this.productosFiltrados = this.productos.filter((producto) => {
       if (this.filters.stock === 'low') {
         return producto.stock < producto.umbral; // Stock bajo
       } else if (this.filters.stock === 'near-threshold') {
         const rangoCercania = producto.umbral * (porcentajeCercania / 100);
         return (
-          producto.stock >= producto.umbral &&
-          producto.stock <= producto.umbral + rangoCercania // Cercano al umbral
+          producto.stock >= producto.umbral && producto.stock <= producto.umbral + rangoCercania // Cercano al umbral
         );
       }
-      return true; // Sin filtros
+      return true; // Sin filtros, mostrar todos los productos
     });
   }
 
   setStockFilter(filter: string): void {
     this.filters.stock = this.filters.stock === filter ? '' : filter;
+    this.filtrarPorStock();
   }
 }
