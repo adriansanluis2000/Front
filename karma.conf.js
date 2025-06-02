9379399ca72e525892fe41cd0a5a8c0d6bcf19d4
adriansanluis2000 <adrian.san-luis@alumnos.unican.es>
2025-06-02 17:05:51 +0200

    Configuración para medir cobertura

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-coverage'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-spec-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            clearContext: false // Deja visible el resultado en navegador
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' },
+                { type: 'lcovonly' }  // útil para VSCode Coverage Gutters
+            ]
+        },
+        reporters: ['progress', 'kjhtml', 'coverage'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        singleRun: false,
+        restartOnFileChange: true
+    });
+};
