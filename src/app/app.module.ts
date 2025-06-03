41a5e9bdca0a46b95ba118b3b843503cd15c1139
adriansanluis2000 <adrian.san-luis@alumnos.unican.es>
2025-06-03 17:43:32 +0200

    Cambio de nombre de la aplicación y añadir fetch a httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { ProductoService } from './services/producto.service';
 import { FormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        BrowserModule,  // Módulo necesario para aplicaciones que corren en un navegador
-        HttpClientModule, // Módulo para realizar peticiones HTTP
-        FormsModule,      // Módulo para trabajar con formularios y validaciones de campos
-        MatSnackBarModule
-    ],
-    providers: [ProductoService], // Servicios que se proveen a toda la aplicación
-    bootstrap: []
+  declarations: [],
+  imports: [
+    BrowserModule, // Módulo necesario para aplicaciones que corren en un navegador
+    HttpClientModule, // Módulo para realizar peticiones HTTP
+    FormsModule, // Módulo para trabajar con formularios y validaciones de campos
+    MatSnackBarModule,
+  ],
+  providers: [ProductoService, provideHttpClient(withFetch())], // Servicios que se proveen a toda la aplicación
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {}
