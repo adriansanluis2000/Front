d3296a37f8359d4d279ba8f0aa376a9bc438edba
adriansanluis2000 <adrian.san-luis@alumnos.unican.es>
2025-07-14 18:16:05 +0200

    Añadir mensaje de éxito/error al agregar un producto

--- src/app/components/agregar-producto/agregar-producto.component.ts
+++ src/app/components/agregar-producto/agregar-producto.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProductoService } from '../../services/producto.service';
 import { FormsModule, FormControl, Validators, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-agregar-producto',
@@ -22,7 +23,7 @@ export class AgregarProductoComponent {
 
   errorMessage: string = '';
 
-  constructor(private readonly productoService: ProductoService) {}
+  constructor(private readonly productoService: ProductoService, private readonly snackBar: MatSnackBar) {}
 
   agregarProducto(form: NgForm) {
     // Restablece el mensaje de error al iniciar la adición de un nuevo producto
@@ -53,47 +54,54 @@ export class AgregarProductoComponent {
     }
 
     if (this.producto.umbral > this.producto.stock) {
-      this.errorMessage =
-        'El umbral mínimo no puede superar la cantidad en stock.';
+      this.errorMessage = 'El umbral mínimo no puede superar la cantidad en stock.';
       return;
     }
 
     // Verificar si el nombre del producto ya existe
-    this.productoService
-      .verificarNombreProducto(this.producto.nombre)
-      .subscribe({
-        next: (exists) => {
-          if (exists) {
-            this.errorMessage =
-              'El nombre del producto ya existe. Por favor, elige otro nombre.';
-            return;
-          }
+    this.productoService.verificarNombreProducto(this.producto.nombre).subscribe({
+      next: (exists) => {
+        if (exists) {
+          this.errorMessage = 'El nombre del producto ya existe. Por favor, elige otro nombre.';
+          return;
+        }
 
-          // Si el formulario es válido y el nombre no existe, continúa con el proceso normal
-          if (form.valid) {
-            this.productoService.crearProducto(this.producto).subscribe({
-              next: (data) => {
-                console.log('Producto añadido:', data);
-                this.resetForm(form);
-              },
-              error: (e) => {
-                if (e.status === 0) {
-                  this.errorMessage =
-                    'Error de conexión. Verifica tu conexión a internet y vuelve a intentarlo.';
-                } else {
-                  this.errorMessage =
-                    'Error al agregar el producto, inténtalo de nuevo.';
-                }
-              },
-            });
-          }
-        },
-        error: (e) => {
-          this.errorMessage =
-            'Error al verificar el nombre del producto. Inténtalo de nuevo.';
-          console.error(e);
-        },
-      });
+        // Si el formulario es válido y el nombre no existe, continúa con el proceso normal
+        if (form.valid) {
+          this.productoService.crearProducto(this.producto).subscribe({
+            next: (data) => {
+              console.log('Producto añadido:', data);
+              this.resetForm(form);
+
+              this.snackBar.open('Producto almacenado con éxito', '', {
+                duration: 3000,
+                horizontalPosition: 'right',
+                verticalPosition: 'top',
+                panelClass: ['snackbar-success'],
+              });
+            },
+            error: (e) => {
+              if (e.status === 0) {
+                this.errorMessage = 'Error de conexión. Verifica tu conexión a internet y vuelve a intentarlo.';
+              } else {
+                this.errorMessage = 'Error al agregar el producto, inténtalo de nuevo.';
+              }
+
+              this.snackBar.open('Error al agregar el producto', '', {
+                duration: 3000,
+                horizontalPosition: 'right',
+                verticalPosition: 'top',
+                panelClass: ['snackbar-error'],
+              });
+            },
+          });
+        }
+      },
+      error: (e) => {
+        this.errorMessage = 'Error al verificar el nombre del producto. Inténtalo de nuevo.';
+        console.error(e);
+      },
+    });
   }
 
   resetForm(form: NgForm) {
